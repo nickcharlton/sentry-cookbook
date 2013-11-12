bf9efb1165c83c8abf9d8251a7dfa92bb89ad5d1
Nick Charlton <hello@nickcharlton.net>
2013-11-12 14:51:21 +0000

    Adds python and postgres dependencies.

--- Berksfile
+++ Berksfile
@@ -1,3 +1,6 @@
 site :opscode
 
+# pull in the custom cookbook first
+cookbook 'postgres', github: 'nickcharlton/postgres-cookbook'
+
 metadata
--- metadata.rb
+++ metadata.rb
@@ -6,3 +6,6 @@ description      'Installs and configures the error logging and aggregation tool
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
+depends 'python'
+depends 'postgres'
+
