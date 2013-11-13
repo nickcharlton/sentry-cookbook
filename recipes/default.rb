1468a4e400cbcfd6d68e9eda867931baf4564f0b
Nick Charlton <hello@nickcharlton.net>
2013-11-13 09:27:52 +0000

    Adds the initial directories, configuration files and install to the recipe.
    And also marks out the next set of steps to implement.

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,3 +6,83 @@
 # 
 # MIT.
 #
+
+include_recipe 'postgres'
+
+# create a user to run the daemon as
+user 'sentry' do
+  home '/home/sentry'
+  shell '/bin/bash'
+  comment 'User for handling the Sentry application'
+  system true
+  supports :manage_home => true
+
+  action :create
+end
+
+# create the application directories
+directory '/var/www/sentry' do
+  owner 'sentry'
+  group 'sentry'
+  mode '0750'
+  recursive true
+end
+
+directory '/etc/sentry' do
+  owner 'sentry' # should be root by with read ability when configured through here
+  group 'sentry'
+  mode '0750'
+end
+
+# create the postgres user and database
+pg_user "#{node['sentry']['database']['user']}" do
+  privileges superuser: false, createdb: false, login: true
+
+  password "#{node['sentry']['database']['password']}"
+end
+
+pg_database "#{node['sentry']['database']['name']}" do
+  owner "#{node['sentry']['database']['user']}"
+  encoding 'utf8'
+  template 'template0'
+  locale 'en_US.UTF8'
+end
+
+# create the virtualenv
+python_virtualenv '/var/www/sentry' do
+  owner 'sentry'
+  group 'sentry'
+  action :create
+end
+
+# install sentry
+%w{sentry[postgres]}.each do |pip|
+  python_pip pip do
+    user 'sentry'
+    group 'sentry'
+    virtualenv '/var/www/sentry'
+  end
+end
+
+# set the configuration file
+template '/etc/sentry/sentry.conf.py' do
+  source 'sentry.conf.py.erb'
+  owner 'sentry'
+  group 'sentry'
+  mode '0770'
+
+  variables(
+    'database' => node['sentry']['database'],
+    'email' => node['sentry']['email'],
+    'secret_key' => ndoe['sentry']['secret_key']
+  )
+end
+
+# run the sentry migration
+
+# configure the superusers
+
+# configure the web server
+
+# apply the nginx settings
+
