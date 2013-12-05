319040ebb547cbf567e1f9d66efce149025d1d6c
Nick Charlton <hello@nickcharlton.net>
2013-12-05 18:03:25 +0000

    Adds attributes for nginx.

--- attributes/default.rb
+++ attributes/default.rb
@@ -7,15 +7,20 @@
 # MIT Licensed.
 #
 
+default['sentry']['nginx']['domain'] = 'sentry.example.com'
+default['sentry']['nginx']['port'] = 80
+
 default['sentry']['database'] = {'name' => '',
                                'user' => '',
                                'password' => ''}
+
 default['sentry']['email'] = {'host' => '',
                               'password' => '',
                               'user' => '',
                               'port' => 25,
                               'use_tls' => 'True',
                               'server_email' => ''}
+
 default['sentry']['secret_key'] = ''
 
 
