c164d354492ecc935ae9dfd85875a74e53ac4443
Nick Charlton <hello@nickcharlton.net>
2013-12-06 09:20:13 +0000

    Bumps the version to 1. Sets the license to MIT.

--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,10 @@
 name             'sentry'
 maintainer       'Nick Charlton'
 maintainer_email 'nick@nickcharlton.net'
-license          'All rights reserved'
+license          'MIT'
 description      'Installs and configures the error logging and aggregation tool, Sentry.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '1.0.0'
 
 depends 'python'
 depends 'postgres'
